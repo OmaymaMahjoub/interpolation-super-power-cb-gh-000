df0e6d225cf62ebcab3f80b0972faa53cf44153d
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-06-28 09:20:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(array=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  if 'red'in array
+    puts "R: red"
+  end
+end  
